c2d645c030ed35c2a6412761c8e130572caf10e4
Pedro Soares <ppsts@iscte-iul.pt>
2026-02-27 15:39:26 +0000

    Update Compass.java
    Adicionei comentários em javadoc a classe compass

--- src/main/java/iscteiul/ista/battleship/Compass.java
+++ src/main/java/iscteiul/ista/battleship/Compass.java
@@ -6,6 +6,12 @@ package iscteiul.ista.battleship;
 /**
  * @author fba
  */
+
+/**
+ * Enumeração que representa as direções possíveis
+ * para orientação dos navios no tabuleiro.
+ */
+
 public enum Compass {
     NORTH('n'), SOUTH('s'), EAST('e'), WEST('o'), UNKNOWN('u');
 
