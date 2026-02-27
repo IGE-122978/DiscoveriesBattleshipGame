5b3d35348c45bf5d036674b04e7c96200d213128
Pedro Soares <ppsts@iscte-iul.pt>
2026-02-27 15:38:06 +0000

    Update Fleet.java

--- src/main/java/iscteiul/ista/battleship/Fleet.java
+++ src/main/java/iscteiul/ista/battleship/Fleet.java
@@ -6,12 +6,6 @@ package iscteiul.ista.battleship;
 import java.util.ArrayList;
 import java.util.List;
 
-/**
- * Representa a frota de um jogador.
- * Responsável por armazenar os navios, validar posicionamentos
- * e verificar colisões e posições ocupadas.
- */
-
 public class Fleet implements IFleet {
     /**
      * This operation prints all the given ships
